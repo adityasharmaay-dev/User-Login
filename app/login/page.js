02c7d687b4bcfa69a6630fd4dae8a11e086e652c
Aditya Sharma <adityasharmaay@gmail.com>
2026-01-30 12:59:25 +0530

    initial nextjs app

---
+++ app/login/page.js
@@ -0,0 +1,8 @@
+"use client";
+
+import { BuilderComponent } from "@builder.io/react";
+import "../../builder";
+
+export default function LoginPage() {
+  return <BuilderComponent model="page" />;
+}
---
+++ builder.js
@@ -0,0 +1,8 @@
+import { builder } from "@builder.io/react";
+
+if (!builder.apiKey) {
+  builder.init("ce331a1e96af4507a2f6ed7bee3b8744");
+}
+
+
+
